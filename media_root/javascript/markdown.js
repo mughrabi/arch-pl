a334c0e749e461b1863d745b33b48d44d5c345ea
Piotr Husiatyński <phusiatynski@gmail.com>
2009-01-30 11:37:42 +0100

    another css & design change

--- media_root/javascript/markdown.js
+++ media_root/javascript/markdown.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
     // markdown info
-    $("#markdown_info").append(
-        "<div style=\"text-align: right; margin: 1em;\"> \
-            <p><em>(<a id=\"markdown_info_more\" href=\"#\">Przykłady użycia</a>.)</em></p> \
-            <div class=\"info\"> \
+    $("#markdown_info").after(
+        "<span id=\"markdown_help\"> \
+            &bull; <a id=\"markdown_info_more\" href=\"#\">Przykłady użycia</a> \
+         </span> \
+         <div id=\"markdown_info_text\"> \
                 <p><em>emfaza</em> - *emfaza*</p> \
                 <p><strong>silna emfaza</strong> - **silna emfaza**</p> \
                 <p><code>kod jednolinijkowy</code> - `kod` </p> \
                 <p><a href=\"#\">link</a> - [nazwa_linka](adres_linka)</p> \
                 <p><em>obrazek</em> - ![nazwa_obrazka](link_obrazka)</p> \
-            </div> \
-        </div> \
+         </div> \
         ");
-    $("#markdown_info").find(".info").hide();
-    $("#markdown_info").click(function() {
-        $(this).find(".info").toggle();
+    $("#markdown_info_text").hide();
+    $("#markdown_help").click(function() {
+        $("#markdown_info_text").toggle();
         return false;
     });
 });
