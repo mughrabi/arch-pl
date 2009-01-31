efe6f1301d1a043486e0effbf72fafac2bb8b6da
Piotr Husiatyński <phusiatynski@gmail.com>
2009-01-31 14:56:21 +0100

    mostly css changes

--- settings.py
+++ settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.humanize',
     'django.contrib.markup',
-    'django.contrib.formtools',
     'forum',
     'userprofile',
     'news',
