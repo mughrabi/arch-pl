93def4752233925eabf37aad8ec8f7e2c1d4d5c5
Piotr Husiatyński <phusiatynski@gmail.com>
2009-03-01 20:24:39 +0100

    new userprofile personalization features; TODO update

--- userprofile/models.py
+++ userprofile/models.py
@@ -2,16 +2,17 @@ import urllib
 import hashlib
 
 from django.db import models
-#from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 from django.utils.safestring import SafeUnicode
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True, null=False)
     jabber = models.EmailField(blank=True)
-    jabber_notify = models.BooleanField(default=False)
+    jabber_notify = models.BooleanField(
+            _("Use jabber notyfications instead of emails"), default=False)
     signature = models.TextField(blank=True)
-    show_signatures = models.BooleanField(default=True)
+    show_signatures = models.BooleanField(_("Show othere users signatures"), default=True)
 
     @property
     def avatar(self):
